9851f390d7351946200da4fc86372c8b8fa6ef05
Wesley <labsfiap@fiap.com.br>
2025-02-25 22:26:16 -0300

    Exercício 1

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 let mensagem: string = "Olá, TypeScript!";
 console.log(mensagem);
+
+// Exercício 1
+let valores: number[] = [10, 20, 30];
+let somaValores: number = valores.reduce((acc, val) => acc + val, 0);
+console.log("Soma dos valores:", somaValores);
