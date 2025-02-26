cb170b5067fc3bfff513feb670b6ed406ee60523
Wesley <labsfiap@fiap.com.br>
2025-02-25 22:28:10 -0300

    Exercício 3

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,13 @@ function mensagemPersonalizada(nome: string): string {
     return `Olá, ${nome}! Bem-vindo ao TypeScript.`;
 }
 console.log(mensagemPersonalizada("Ana"));
+
+// Exercício 3
+class Carro {
+    constructor(public marca: string, public modelo: string, public ano: number) {}
+    detalhes(): string {
+        return `${this.marca} ${this.modelo}, Ano: ${this.ano}`;
+    }
+}
+const carro1 = new Carro("Toyota", "Corolla", 2022);
+console.log(carro1.detalhes());
