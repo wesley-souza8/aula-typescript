1491ca25bfaa1cb5ee4065e91c7ed4eb86bc2b49
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-07 22:12:24 -0300

    Exercício 2 e 3

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,15 @@ console.log(carro1.detalhes());
 
 // Aula de 07/03/2025
 
-// Exercício 1
+/*
+Exercício 1
+Crie uma interface chamada Carro, que contenha as seguintes propriedades:
+marca (string)
+modelo (string)
+ano (number)
+motor (opcional, string)
+Em seguida, crie um objeto chamado meuCarro que implemente essa interface e exiba os dados no console.
+*/
 interface Carros {
     marca: string;
     modelo: string;
@@ -40,3 +48,47 @@ const meuCarro: Carros = {
 };
 console.log(meuCarro)
 
+/*
+Exercício 2
+Crie uma interface Multiplicacao para tipar uma função que recebe dois números e retorna o resultado da multiplicação deles.
+Depois, implemente essa função e teste com diferentes valores.
+*/
+interface Multiplicacao {
+    (a: number, b: number): number;
+}
+const multiplica: Multiplicacao = (x, y) => x * y;
+console.log(multiplica(5,10));
+
+/*
+Exercício 3
+Crie uma função genérica chamada inverterArray<T>, que recebe um array de qualquer tipo e retorna um novo array com os elementos invertidos.
+Teste a função com um array de números e um array de strings.
+*/
+
+function inverterArray<T>(array: T[]): T[] {
+    return array.reverse();
+}
+
+const listaNumero: number[] = [10,9,8,7,6,5,4,3,2,1];
+console.log(inverterArray(listaNumero));
+
+const listaPalavras: string[] = ["a","b","c","d"];
+console.log(inverterArray(listaPalavras));
+
+/*
+Exercício 4
+Crie uma interface genérica chamada Repositorio<T>, que tenha um método salvar(dado: T): void e um método obterTodos(): T[].
+Depois, crie uma implementação dessa interface para armazenar uma lista de usuários (com nome e email).
+*/
+
+/*
+Exercício 5
+Crie um Type Alias chamado RespostaServidor, que pode ser uma string ou um boolean.
+Depois, crie uma função chamada processarResposta que recebe um valor desse tipo e imprime uma mensagem no console dependendo do valor passado.
+*/
+
+/*
+Exercício 6
+Utilizando Intersection Types, crie um tipo EstudanteTrabalhador, que una as propriedades de um Estudante (nome e curso) e de um Trabalhador (empresa e cargo).
+Depois, crie um objeto que represente um estudante que também trabalha e exiba os dados no console.
+*/
